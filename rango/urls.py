38a13952764caedd6f7724de2ba9f860eb9a90ff
Andrew Mclellan <2317717M@student.gla.ac.uk>
2021-01-21 18:27:25 +0000

    Chapter 3 Commit

---
+++ rango/urls.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 21 18:10:56 2021
+
+@author: Andrew
+"""
+
+from django.urls import path
+from rango import views
+
+app_name = 'rango'
+
+urlpatterns = [
+    path('', views.index, name='index'),
+]
